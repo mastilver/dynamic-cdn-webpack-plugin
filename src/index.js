338e162501054b1fd3cd27de31d7a7049a425971
Bala Bosch <moonray@users.noreply.github.com>
2018-09-26 11:37:07 -0400

    fix: exports not working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 export {default as HtmlDynamicCdnWebpackPlugin} from './html-dynamic-cdn-webpack-plugin';
-export {default} from './dynamic-cdn-webpack-plugin';
+export {default as DynamicCdnWebpackPlugin} from './dynamic-cdn-webpack-plugin';
